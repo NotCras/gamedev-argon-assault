2085c48f402d093df3b9d59c7ff5d4cebee2aaaf
John <morrowjo@oregonstate.edu>
2020-04-09 16:46:44 -0700

    worked up to quiz 1

---
+++ Assets/splashScreen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class splashScreen : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LoadFirstLevel();
+    }
+
+    private static void LoadFirstLevel()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
