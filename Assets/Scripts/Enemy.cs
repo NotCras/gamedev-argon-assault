6dc573baf5664e775ae11a7b6a8a6cb07b37a43a
John <morrowjo@oregonstate.edu>
2020-04-15 14:26:59 -0700

    scoreboard!

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,21 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject deathFX;
     [SerializeField] private Transform parent;
+    [SerializeField] private int scorePerHit = 12; //maybe put it on the enemy?
+
+
+    private ScoreBoard scoreBoard;
     
     // Start is called before the first frame update
     void Start()
     {
         AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     private void OnParticleCollision(GameObject other)
     {
         GameObject explosion = Instantiate(deathFX, transform.position, Quaternion.identity);
         explosion.transform.parent = parent;
+        scoreBoard.ScoreHit(scorePerHit);
         Destroy(this.gameObject);
     }
 
